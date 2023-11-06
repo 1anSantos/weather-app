83417eeb0af3e4bf8b5851d2241abb2f2ad264f3
Ian Santos <64675360+1anSantos@users.noreply.github.com>
2023-11-06 14:14:14 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,8 +4,24 @@ const apiWeatherKey = "c06d17ba99219c0efb01f7f9a77032fd";
 // Elementos HTML
 const cityInput = document.querySelector("#city-input");
 const getWeatherBtn = document.querySelector("#get-weather");
-const weatherResult = document.querySelector("#weather-result");
 
+// Elementos HTML
+// Cidade e País
+const city = document.querySelector("#city");
+const country = document.querySelector("#country");
+const countryImg = document.querySelector("#country-img");
+
+// Temperatura e Clima
+const weatherImgContainer = document.querySelector("#weather-img-container");
+const weatherImg = document.querySelector("#weather-img");
+const temperature = document.querySelector("#temperature");
+const description = document.querySelector("#description");
+
+// Informações Adicionais
+const fellsLike = document.querySelector("#fells-like .information span");
+const cloud = document.querySelector("#cloud .information span");
+const humidity = document.querySelector("#humidity .information span");
+const wind = document.querySelector("#wind .information span");
 
 // Event Listeners
 cityInput.addEventListener("keydown", (event) =>{
@@ -20,27 +36,32 @@ getWeatherBtn.addEventListener("click", getWeatherByAPI);
 function getWeatherByAPI() {
     const city = cityInput.value;
     const apiWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiWeatherKey}&limit=5&lang=pt_br&units=metric`;
-    
+    cityInput.value = "";
+
     fetch(apiWeatherUrl)
         .then(response => response.json())
         .then(data => {
             getCountryName(data.sys.country)
                 .then(countryName => {
-                    console.log(data)
-                    weatherResult.innerHTML = `
-                        <div style="display: flex; align-items: center">
-                            <img src="https://flagcdn.com/h60/${data.sys.country.toLowerCase()}.png" alt="${countryName}">
-                            <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png">
-                        </div>
-                        <h2 class="display-3">Clima em ${data.name}, ${countryName}</h2>
-                        
-                        <p>Tempo: ${data.weather[0].description}</p>
-                        <p>Umidade: ${data.main.humidity}%</p>
-                        <p>Temperatura: ${data.main.temp}°C</p>
-                        <p>Sensação Térmica: ${data.main.feels_like}°C</p>
-                        <p>Vento: ${data.wind.speed}m/s</p>
-                        <p>Nuvens: ${data.clouds.all}%</p>
-                        `;
+                    // mostrar no console
+                    weatherImgContainer.style.display = "block";
+
+                    // Cidade, País e Bandeira
+                    city.innerHTML = data.name;
+                    country.innerHTML = countryName;
+                    countryImg.src = `https://flagcdn.com/h20/${data.sys.country.toLowerCase()}.png`;
+                    
+                    // Icone do Clima, Temperatura e clima
+                    weatherImg.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`;
+                    temperature.innerHTML = `${Math.round(data.main.temp)}<span>°C</span>`;
+                    description.innerHTML = data.weather[0].description;
+                    
+                    // Informações adicionais
+                    fellsLike.innerHTML = `${Math.round(data.main.feels_like)} °C`;
+                    cloud.innerHTML = `${data.clouds.all}%`;
+                    humidity.innerHTML = `${data.main.humidity}%`;
+                    wind.innerHTML = `${data.wind.speed} m/s`;
+
                 })
                 .catch(error => {
                     console.error('Ocorreu um erro ao obter o nome do país:', error);
